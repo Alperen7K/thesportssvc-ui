eaae485ff5c693d27144383ab9099c0a84d8f55e
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-11-26 15:42:07 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./thesportssvc-logo.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <div className="bg-[#202429] w-screen h-screen text-white flex flex-col md:flex-row items-center justify-evenly md:justify-around p-4">
+   <div className="flex items-center justify-center">
+    <img
+     src={logo}
+     className="h-[40vh] md:h-fit md:w-[30vw] rounded-[50px] mx-auto shadow-md shadow-[#deb9846f]"
+    />
+   </div>
+   <div className="h-[40vh] md:h-fit md:w-[30vw] flex flex-col items-center justify-center gap-y-[5vh]">
+    <h1 className="text-[40px] md:text-[50px] text-center ">
+     Only <b>sportstech</b> <br />
+     startups can apply.
+    </h1>
+    <a
+     className="text-[24px] md:text-[36px] px-4 py-2 rounded-[20px] shadow-md hover:shadow-lg duration-300   hover:shadow-[#deb9846f]  shadow-[#deb9846f]  bg-[#202429] "
+     href="mailto:info@thesportsvc.com"
+    >
+     info@<b>thesportsvc</b>.com
+    </a>
+   </div>
+  </div>
+ );
 }
 
 export default App;
